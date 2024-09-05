968e6d00aae1baae70a86c0a9cb2ed218d16617a
kblatnik <kevin-blatnik@users.noreply.github.com>
2024-09-05 12:50:36 -0700

    load images into ai/player frames

--- javascript.js
+++ javascript.js
@@ -43,33 +43,67 @@ function hasScrolled() {
 }
 
 var playerFrame = document.getElementById("playerImage");
+var aiFrame = document.getElementById("aiImage");
 var isPlayerMove = true;
+var turn = 1;
+var randTurn = 0;
+var playerScore = 0;
+var aiScore = 0;
+
+const array = new Uint32Array(100);
+self.crypto.getRandomValues(array);
+
+// for (const num of array) {
+//   console.log(num);
+// }
 
 document.getElementById("btnRock").addEventListener('click', function() {
     if(isPlayerMove) {
-        // isPlayerMove = false;
+        chooseBlank(aiFrame);
+        isPlayerMove = false;
         chooseRock(playerFrame);
+        aiMove();
     }
 });
 document.getElementById("btnPaper").addEventListener('click', function() {
     if(isPlayerMove) {
-        // isPlayerMove = false;
+        chooseBlank(aiFrame);
+        isPlayerMove = false;
         choosePaper(playerFrame);
+        aiMove();
     }
 });
 document.getElementById("btnScissors").addEventListener('click', function() {
     if(isPlayerMove) {
+        chooseBlank(aiFrame);
         isPlayerMove = false;
         chooseScissors(playerFrame);
-        pause();
-        isPlayerMove = true;
+        aiMove();
     }
 });
-const delay = ms => new Promise(res => setTimeout(res, ms));
+
 const pause = async () => {
+    const delay = ms => new Promise(res => setTimeout(res, ms));
     await delay(5000);
     console.log("Waited 5s");
 };
+
+function aiMove() {
+    var move = array[randTurn] % 3;
+    console.log(move);
+    switch(move) {
+        case 0:
+            chooseRock(aiFrame);
+            break;
+        case 1:
+            choosePaper(aiFrame);
+            break;
+        case 2:
+            chooseScissors(aiFrame);
+    }
+    isPlayerMove = true;
+    randTurn += 1;
+}
 
 function chooseRock(frame) {
     frame.setAttribute('src', './images/pexels-pixabay-161702.jpg')
@@ -79,4 +113,7 @@ function choosePaper(frame) {
 }
 function chooseScissors(frame) {
     frame.setAttribute('src', 'images/pexels-jessbaileydesign-755991.jpg')
+}
+function chooseBlank(frame) {
+    frame.setAttribute('src', '')
 }
